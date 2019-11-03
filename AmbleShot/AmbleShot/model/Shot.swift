077d45634760783c2956cc3cea518c4a7556c699
Pavel Zak <izakpavel@gmail.com>
2019-11-03 09:44:20 +0100

    added Codable to ShotState

--- AmbleShot/AmbleShot/model/Shot.swift
+++ AmbleShot/AmbleShot/model/Shot.swift
@@ -8,15 +8,53 @@
 
 import Foundation
 
-class Shot : Identifiable, Codable{
+enum ShotState : Int, Codable {
+    case justLocation = 0
+    case loading = 1 // loading of image description and downloading of data is understood as a single operation
+    case loaded = 2
+}
+
+class Shot : Identifiable, Codable, ObservableObject{
     let id: Int // unique identifier
     let lat: Double
     let lng: Double
+    var imageUrl: String?
+    @Published var state : ShotState
     
     init(id: Int, lat: Double, lng: Double) {
         self.id = id
         self.lat = lat
         self.lng = lng
+        self.imageUrl = nil
+        self.state = .justLocation
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case lat
+        case lng
+        case imageUrl
+        case state
+    }
+    
+    required init(from decoder: Decoder) throws{
+    
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        
+        self.id = try values.decode(Int.self, forKey: .id)
+        self.lat = try values.decode(Double.self, forKey: .lat)
+        self.lng = try values.decode(Double.self, forKey: .lng)
+        self.imageUrl = try values.decode(String.self, forKey: .imageUrl)
+        self.state = try values.decode(ShotState.self, forKey: .state)
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(id, forKey: .id)
+        try container.encode(lat, forKey: .lat)
+        try container.encode(lng, forKey: .lng)
+        try container.encode(imageUrl, forKey: .imageUrl)
+        try container.encode(state, forKey: .state)
     }
     
     func imageName()->String {
